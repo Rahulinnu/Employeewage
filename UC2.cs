f5b9f56054f656732a5eadc3f7eb2a7257c1b30c
Rahulinnu <101334027+Rahulinnu@users.noreply.github.com>
2022-03-24 03:30:11 +0530

    Add files via upload

---
+++ UC2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmpWage
+{
+    public class UC2
+    {
+        public UC2()
+        {
+            Console.WriteLine("Welcome to Employee Wage Compuatation Program\n");
+
+            const int isPresent = 1;
+            const int wagePerHour = 20;
+            int empWorkHour = 0;
+            int empDailyWage = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+                empWorkHour = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empWorkHour = 0;
+            }
+            empDailyWage = empWorkHour * wagePerHour;
+            Console.WriteLine($"\nEmployee Daily Wage : {empDailyWage}\n");
+            Console.ReadLine();
+        }
+    }
+}
