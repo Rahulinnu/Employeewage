f6a02e64efb177b317b4f6a5cb40fe1200aa9f55
rahul unni <rahul@rahulsmac.local>
2022-03-25 22:10:48 +0530

    UC6 added

--- EmpWage/UC6.cs
+++ EmpWage/UC6.cs
@@ -1,10 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace EmpWage
 {
     public class UC6
     {
         public UC6()
         {
+            Console.WriteLine("Welcome to Employee Wage Compuatation Program\n");
+
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int WAGE_PER_HOUR = 20;
+            const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORK_HOURS = 100;
+            int totalMonthWage = 0, empWorkHour = 0, empDailyWage = 0, empTotalHour = 0, empTotalWorkDays = 0;
+
+            Random random = new Random();
+            while (empTotalHour <= MAX_WORK_HOURS && empTotalWorkDays <= MAX_WORKING_DAYS)
+            {
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Employee is Present for Full Time");
+                        empWorkHour = 8;
+                        break;
+
+                    case IS_PART_TIME:
+                        Console.WriteLine("Employee is Present for Part Time");
+                        empWorkHour = 4;
+                        break;
+
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        empWorkHour = 0;
+                        break;
+                }
+
+
+                empDailyWage = empWorkHour * WAGE_PER_HOUR;
+                Console.WriteLine($"Employee Daily Wage for Day {empTotalWorkDays} : {empDailyWage}\n");
+                totalMonthWage += empDailyWage;
+                empTotalWorkDays++;
+                empTotalHour += empWorkHour;
+
+            }
+            if (empTotalHour > MAX_WORK_HOURS)
+            {
+                int a = empTotalHour - MAX_WORK_HOURS;
+                empTotalHour -= a;
+                int wage = a * WAGE_PER_HOUR;
+                totalMonthWage -= wage;
+            }
+
+            if (empTotalWorkDays > MAX_WORKING_DAYS)
+            {
+                empTotalWorkDays -= 1;
+            }
+            Console.WriteLine($"\nEmployee total working days  : {empTotalWorkDays}");
+            Console.WriteLine($"Employee total working hours  : {empTotalHour}\n");
+            Console.WriteLine($"\nEmployee Total Month Wage : {totalMonthWage}\n");
+            Console.ReadLine();
         }
     }
 }
